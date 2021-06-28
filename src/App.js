692a01ce326bb699bf20fca96d2f934b692fecc6
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-06-28 08:32:57 +0530

    change in ui, now, using react hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React,{Component} from 'react';
+import React,{ useState } from 'react';
 import District from './District/District';
 import process from 'process';
-class App extends Component
+
+export default function App()
 {
-  state = {
+  const [state, setState] = useState({
     loading:false,
     fetched:false,
     err:false,
@@ -11,90 +12,80 @@ class App extends Component
     active:"-1",
     recovered:"-1",
     deaths:"-1",
-    total:"-1"
-  }
-  getData(dist)
-  {
-    // let url = "https://node-py-1.herokuapp.com/api/districts/"+dist
-    // now use aws hosted api
-    let url = "http://ec2-65-2-140-32.ap-south-1.compute.amazonaws.com:3333/api/districts/"+dist
-    
-    if(process.env.NODE_ENV==='development')
-      url="http://localhost:3333/api/districts/"+dist;
-    fetch(url)
-      .then(res=>{
-        return res.json();
-      })
-      .then(data=>{
-        this.setState(
-          {
-            loading:false,
-            fetched:true,
-            err:false,
-            name:data.name,
-            active:data.active,
-            recovered:data.recovered,
-            deaths:data.deaths,
-            total:data.total
-          });
-      })
-      .catch(err=>{
-        this.setState(
-          {fetched:true,err:true,loading:false});
+    total:"-1",
+  });
+  
+function getData(dist) {
+  // let url = "https://node-py-1.herokuapp.com/api/districts/"+dist
+  // now use aws hosted api
+  let url = "http://ec2-65-2-140-32.ap-south-1.compute.amazonaws.com:3333/api/districts/"+dist;
+  
+  if(!process.env.NODE_ENV || process.env.NODE_ENV==='development')
+    url="http://localhost:3333/api/districts/"+dist;
+  // console.log(url);
+  fetch(url)
+    .then(res=>{
+      return res.json();
+    })
+    .then(data=>{
+      setState({
+        ...state,
+        loading:false,
+        fetched:true,
+        err:false,
+        name:data.name,
+        active:data.active,
+        recovered:data.recovered,
+        deaths:data.deaths,
+        total:data.total
       });
-  }
-
-  btnClickedCount=0;
-  buttonClickedHandler = (event) => {
-    let dist = document.getElementById('input').value;
-    if(dist==="")
-    {
-      this.setState({err:true,fetched:true,loading:false});
-      return;
-    }
-    ++this.btnClickedCount;
-    this.setState({loading:true});
-    if(this.btnClickedCount>1)
-    {
-      setTimeout(()=>{
-        this.getData(dist);
-      },600);
-    }
-    else
-      this.getData(dist);
-  }
-  render()
-  {
-    
-    const {loading,fetched,err,name,active,recovered,deaths,total}=this.state;
-    
-    if(!fetched)
-    {
-      return(
-            <District
-              error = {err}
-              loading = {loading}
-              click={(event)=>this.buttonClickedHandler(event)}
-            />
-      );
-    }
-    else
-    {
-      return(
-          <District
-            error = {err}
-            loading={loading}
-            fetched={fetched}
-            active={active}
-            recovered={recovered}
-            deaths={deaths}
-            total={total}
-            click={(event)=>this.buttonClickedHandler(event)}
-            
-          />
-      );
-    }
-  }
+    })
+    .catch(err=>{
+      setState(
+        {...state, fetched:true,err:true,loading:false});
+    });
 }
 
-export default App;
+  const buttonClickedHandler = (dist) => {
+    if(dist==="Select"){
+      return;
+    }
+    else if(dist==="")
+    {
+      setState({...state, err:true,fetched:true,loading:false});
+      return;
+    }
+    // setButtonClickedCount(setButtonClickedCount+1);
+    setState({...state, loading:true});
+    getData(dist);
+  }
+
+  const {loading,fetched,err,name,active,recovered,deaths,total}=state;
+  
+  if(!fetched)
+  {
+    return(
+          <District
+            error = {err}
+            loading = {loading}
+            click={(districtName)=>buttonClickedHandler(districtName)}
+          />
+    );
+  }
+  else
+  {
+    return(
+        <District
+          error = {err}
+          loading={loading}
+          fetched={fetched}
+          active={active}
+          recovered={recovered}
+          deaths={deaths}
+          total={total}
+          click={buttonClickedHandler}
+        />
+    );
+  }
+  
+}
--- src/District/District.js
+++ src/District/District.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from '@material-ui/core/Paper';
@@ -11,12 +10,17 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const district = (props) => {
+export default function District(props) {
+  const [districtName, setDistrictName] = useState('Select');
+  const handleChange = (event) => {
+    setDistrictName(event.target.value);
+  };
+
   let outputData = null;
-  
-  if(props.loading)
-  {
+  if(props.loading) {
     outputData = (
       <Container>
         <CircularProgress size={20} style={{margin:10}} />
@@ -58,16 +62,95 @@ const district = (props) => {
       </Container>
     );
   }
+
   return (
     <Container maxWidth = "lg" style={{textAlign:"center"}} >
       <Grid item xs={12}>
         <Paper xs={12} style={{textAlign:"center",margin:10}} >
           <Container maxWidth="lg">
-            <TextField
-              style={{marginTop:10}}
+            <Select
+              labelId="District"
               id="input"
-              label="District"
-            />
+              value={districtName}
+              onChange={handleChange}
+            >
+              <MenuItem value={'Select'}>Select</MenuItem>
+              <MenuItem value={'Agra'}>Agra</MenuItem>
+              <MenuItem value={'Aligarh'}>Aligarh</MenuItem>
+              <MenuItem value={'Ambedkar Nagar'}>Ambedkar Nagar</MenuItem>
+              <MenuItem value={'Amethi'}>Amethi</MenuItem>
+              <MenuItem value={'Amroha'}>Amroha</MenuItem>
+              <MenuItem value={'Auraiya'}>Auraiya</MenuItem>
+              <MenuItem value={'Ayodhya'}>Ayodhya</MenuItem>
+              <MenuItem value={'Azamgarh'}>Azamgarh</MenuItem>
+              <MenuItem value={'Baghpat'}>Baghpat</MenuItem>
+              <MenuItem value={'Bahraich'}>Bahraich</MenuItem>
+              <MenuItem value={'Ballia'}>Ballia</MenuItem>
+              <MenuItem value={'Balrampur'}>Balrampur</MenuItem>
+              <MenuItem value={'Banda'}>Banda</MenuItem>
+              <MenuItem value={'Barabanki'}>Barabanki</MenuItem>
+              <MenuItem value={'Bareilly'}>Bareilly</MenuItem>
+              <MenuItem value={'Basti'}>Basti</MenuItem>
+              <MenuItem value={'Bhadohi'}>Aligarh</MenuItem>
+              <MenuItem value={'Bijnor'}>Bijnor</MenuItem>
+              <MenuItem value={'Budaun'}>Budaun</MenuItem>
+              <MenuItem value={'Bulandshahr'}>Bulandshahr</MenuItem>
+              <MenuItem value={'Chandauli'}>Chandauli</MenuItem>
+              <MenuItem value={'Chitrakoot'}>Chitrakoot</MenuItem>
+              <MenuItem value={'Deoria'}>Deoria</MenuItem>
+              <MenuItem value={'Etah'}>Etah</MenuItem>
+              <MenuItem value={'Etawah'}>Etawah</MenuItem>
+              <MenuItem value={'Farrukhabad'}>Farrukhabad</MenuItem>
+              <MenuItem value={'Fatehpur'}>Fatehpur</MenuItem>
+              <MenuItem value={'Firozabad'}>Firozabad</MenuItem>
+              <MenuItem value={'Gautam Buddha Nagar'}>Gautam Buddha Nagar</MenuItem>
+              <MenuItem value={'Ghaziabad'}>Ghaziabad</MenuItem>
+              <MenuItem value={'Ghazipur'}>Ghazipur</MenuItem>
+              <MenuItem value={'Gonda'}>Gonda</MenuItem>
+              <MenuItem value={'Gorakhpur'}>Gorakhpur</MenuItem>
+              <MenuItem value={'Hamirpur'}>Hamirpur</MenuItem>
+              <MenuItem value={'Hapur'}>Hapur</MenuItem>
+              <MenuItem value={'Hardoi'}>Hardoi</MenuItem>
+              <MenuItem value={'Hathras'}>Hathras</MenuItem>
+              <MenuItem value={'Jalaun'}>Jalaun</MenuItem>
+              <MenuItem value={'Jaunpur'}>Jaunpur</MenuItem>
+              <MenuItem value={'Jhansi'}>Jhansi</MenuItem>
+              <MenuItem value={'Kannauj'}>Kannauj</MenuItem>
+              <MenuItem value={'Kanpur Dehat'}>Kanpur Dehat</MenuItem>
+              <MenuItem value={'Kanpur Nagar'}>Kanpur Nagar</MenuItem>
+              <MenuItem value={'Kasganj'}>Kasganj</MenuItem>
+              <MenuItem value={'Kaushambi'}>Kaushambi</MenuItem>
+              <MenuItem value={'Kushinagar'}>Kushinagar</MenuItem>
+              <MenuItem value={'Lakhimpur Kheri'}>Lakhimpur Kheri</MenuItem>
+              <MenuItem value={'Lalitpur'}>Lalitpur</MenuItem>
+              <MenuItem value={'Lucknow'}>Lucknow</MenuItem>
+              <MenuItem value={'Maharajganj'}>Maharajganj</MenuItem>
+              <MenuItem value={'Mahoba'}>Mahoba</MenuItem>
+              <MenuItem value={'Mainpuri'}>Mainpuri</MenuItem>
+              <MenuItem value={'Mathura'}>Mathura</MenuItem>
+              <MenuItem value={'Mau'}>Mau</MenuItem>
+              <MenuItem value={'Meerut'}>Meerut</MenuItem>
+              <MenuItem value={'Mirzapur'}>Mirzapur</MenuItem>
+              <MenuItem value={'Moradabad'}>Moradabad</MenuItem>
+              <MenuItem value={'Muzaffarnagar'}>Muzaffarnagar</MenuItem>
+              <MenuItem value={'Pilibhit'}>Pilibhit</MenuItem>
+              <MenuItem value={'Pratapgarh'}>Pratapgarh</MenuItem>
+              <MenuItem value={'Prayagraj'}>Prayagraj</MenuItem>
+              <MenuItem value={'Rae Bareli'}>Rae Bareli</MenuItem>
+              <MenuItem value={'Rampur'}>Rampur</MenuItem>
+              <MenuItem value={'Sharanpur'}>Sharanpur</MenuItem>
+              <MenuItem value={'Sambhal'}>Sambhal</MenuItem>
+              <MenuItem value={'Sant Kabir Nagar'}>Sant Kabir Nagar</MenuItem>
+              <MenuItem value={'Shahjahanpur'}>Shahjahanpur</MenuItem>
+              <MenuItem value={'Shamli'}>Shamli</MenuItem>
+              <MenuItem value={'Shrawasti'}>Shrawasti</MenuItem>
+              <MenuItem value={'Siddharthnagar'}>Siddharthnagar</MenuItem>
+              <MenuItem value={'Sitapur'}>Sitapur</MenuItem>
+              <MenuItem value={'Sonbhadra'}>Sonbhadra</MenuItem>
+              <MenuItem value={'Sultanpur'}>Sultanpur</MenuItem>
+              <MenuItem value={'Unnao'}>Unnao</MenuItem>
+              <MenuItem value={'Varanasi'}>Varanasi</MenuItem>
+            </Select>
           </Container >
           {outputData}
           <Container maxWidth="lg">
@@ -75,15 +158,12 @@ const district = (props) => {
               style={{marginBottom:15, marginTop:15}}
               variant="outlined"
               color="default"
-              onClick={ props.click }
+              onClick={(e)=> props.click(districtName) }
             > Submit
             </Button >
           </Container>
           </Paper>
-        </Grid>
-      
+        </Grid>      
     </Container>
   );
-};
-
-export default district;
+}
